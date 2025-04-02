f3930c431e6140c58f6c0b11f753aee5a6322a71
Gabriel Tomazini Marani <gabrieltm.ifms@gmail.com>
2025-04-01 22:50:21 -0300

    1st commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { InitiativeComponent } from './initiative/initiative.component';
+
+export const routes: Routes = [{ path: '', component: InitiativeComponent }];
