180f155e0a003752147fbaebf2f626543713b6e8
Gabriel Tomazini Marani <gabrieltm.ifms@gmail.com>
2025-04-03 16:32:26 -0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { InitiativeComponent } from './initiative/initiative.component';
-
-export const routes: Routes = [{ path: '', component: InitiativeComponent }];
